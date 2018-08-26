8b84b8cdc112fdd48164952cd0710f28c247c9c1
MadCoderr <msrfarooqi@gmail.com>
2018-08-26 13:08:21 +0500

    Added Player Movement feature

---
+++ Bug Rescue/Assets/_Scripts/PlayerBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private float Speed = 3f;
+
+    private Rigidbody _rbPlayer;
+
+	void Start () {
+        _rbPlayer = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        playerMovement();
+    }
+
+    // A funciton to define movement for player 
+    private void playerMovement() {
+        var horizontal = Input.GetAxis("Horizontal"); // gettting x-axis
+        // this will let the player to move on x-axis
+        _rbPlayer.velocity = new Vector3(-horizontal * Speed, _rbPlayer.velocity.y, 0);
+    }
+
+}
