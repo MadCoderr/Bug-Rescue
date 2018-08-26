b772831fc299361a3cdafa4d70f67888ac9928e6
Raapchik <priyankdegami1@gmail.com>
2018-08-26 14:41:25 +0530

    Improved Assets and a Gate
    Just to try out Openeing flower mechanic.

--- Bug Rescue/Assets/_Scripts/CameraFollow.cs
+++ Bug Rescue/Assets/_Scripts/CameraFollow.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 
     [SerializeField]
-    private Transform FollowTarget;
+	private Transform FollowTarget;
 
-    [SerializeField]
+	[SerializeField]
+	private float camSpeed;
+
     private Vector3 OffSet;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		OffSet = new Vector3 (0, 4, 10);
 	}
 	
 	// Update is called once per frame
-	void LateUpdate () {
-        transform.position = FollowTarget.position + OffSet;
+	void LateUpdate ()
+	{
+      //  transform.position = FollowTarget.position + OffSet;
+		transform.position = Vector3.Lerp (transform.position, FollowTarget.position+new Vector3(0,4,10),camSpeed);  //just to smooth.
+		//transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
 	}
 }
--- Bug Rescue/Assets/_Scripts/PlayerBehaviour.cs
+++ Bug Rescue/Assets/_Scripts/PlayerBehaviour.cs
@@ -9,6 +9,8 @@ public class PlayerBehaviour : MonoBehaviour {
 
     private Rigidbody _rbPlayer;
 
+	public ExampleBridge refer;
+
 	void Start () {
         _rbPlayer = GetComponent<Rigidbody>();
 	}
@@ -27,6 +29,10 @@ public class PlayerBehaviour : MonoBehaviour {
         var horizontal = Input.GetAxis("Horizontal"); // gettting x-axis
         // this will let the player to move on x-axis
         _rbPlayer.velocity = new Vector3(-horizontal * Speed, _rbPlayer.velocity.y, 0);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			refer.GetComponent<ExampleBridge> ().enabled = true;  // When user gives input of space We activate its script component to open the gate;(By default the gate is closed)
+		}
     }
 
 }
